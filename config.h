feadca998991766dfd6b733b38fe4c692b2ec4e6
sherlock9c1 <joel_banyai@hotmail.com>
2022-01-03 06:51:02 -0600

    initial baseline in Git is 2.0
    Updated per steveo's suggestion. From here we can fork to start work on the '96-97 code.

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define FLASHHACK_VERSION "1.4"
+#define FLASHHACK_VERSION "2.0"
+//note: 1/3/22 sherlock9c1, updated from v1.4 per recommendation from steveo
 
 #endif // CONFIG_H
